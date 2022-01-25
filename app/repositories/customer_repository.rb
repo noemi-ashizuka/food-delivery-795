4ec25059438d5f37dafec4bdbae5fad06a23915c
Noemi Ashizuka <noemi.calanducci@gmail.com>
2022-01-25 18:56:09 +0900

    Inheritance for Meal and Customer

--- app/repositories/customer_repository.rb
+++ app/repositories/customer_repository.rb
@@ -1,41 +1,10 @@
 require 'csv'
 require_relative '../models/customer'
 
-class CustomerRepository
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
-    @next_id = 1
-    @customers = []
-    load_csv
-  end
+class CustomerRepository < BaseRepository
 
-  def all
-    @customers
-  end
-
-  def add(customer)
-    customer.id = @next_id
-    @customers << customer
-    @next_id += 1
-    save_csv
-  end
-
-  private
-
-  def load_csv
-    CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |attributes|
-      attributes[:id] = attributes[:id].to_i
-      @customers << Customer.new(attributes)
-    end
-    @next_id = @customers.last.id + 1 unless @customers.empty?
-  end
-
-  def save_csv
-    CSV.open(@csv_file_path, "wb") do |csv|
-      csv << ['id', 'name', 'address']
-      @customers.each do |customer|
-        csv << [customer.id, customer.name, customer.address]
-      end
-    end
+  def build_element(row)
+    row[:id] = row[:id].to_i
+    Customer.new(row)
   end
 end
--- app/repositories/meal_repository.rb
+++ app/repositories/meal_repository.rb
@@ -1,42 +1,12 @@
 require 'csv'
 require_relative '../models/meal'
+require_relative 'base_repository'
 
-class MealRepository
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
-    @next_id = 1
-    @meals = []
-    load_csv
-  end
+class MealRepository < BaseRepository
 
-  def all
-    @meals
-  end
-
-  def add(meal)
-    meal.id = @next_id
-    @meals << meal
-    @next_id += 1
-    save_csv
-  end
-
-  private
-
-  def load_csv
-    CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |attributes|
-      attributes[:id] = attributes[:id].to_i
-      attributes[:price] = attributes[:price].to_i
-      @meals << Meal.new(attributes)
-    end
-    @next_id = @meals.last.id + 1 unless @meals.empty?
-  end
-
-  def save_csv
-    CSV.open(@csv_file_path, "wb") do |csv|
-      csv << ['id', 'name', 'price']
-      @meals.each do |meal|
-        csv << [meal.id, meal.name, meal.price]
-      end
-    end
+  def build_element(row)
+    row[:id] = row[:id].to_i
+    row[:price] = row[:price].to_i
+    Meal.new(row)
   end
 end
